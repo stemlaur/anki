4928e0c2f19aff676624349ced790b49e59fe8a4
laurent stemmer <laurent.stemmer@weekendesk.fr>
2019-11-09 17:49:00 +0100

    Set-up the project

---
+++ src/main/java/com/stemlaur/anki/domain/package-info.java
@@ -0,0 +1,1 @@
+package com.stemlaur.anki.domain;
---
+++ src/test/java/com/stemlaur/anki/domain/CanaryTest.java
@@ -0,0 +1,12 @@
+package com.stemlaur.anki.domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class CanaryTest {
+    @Test
+    public void canaryTest() {
+        assertTrue(true);
+    }
+}
