4fbc5ed855bcc5248d2868e9227beeea73a83831
laurent stemmer <laurent.stemmer@weekendesk.fr>
2019-11-19 13:12:12 +0100

    Refactor tests to use Should and proper case

--- src/test/java/com/stemlaur/anki/domain/DeckServiceShould.java
+++ src/test/java/com/stemlaur/anki/domain/DeckServiceShould.java
@@ -12,35 +12,35 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
-public final class DecksTest {
-    private Decks decks;
+public final class DeckServiceShould {
+    private DeckService deckService;
     @Mock
     private DeckRepository deckRepository;
 
     @Before
     public void setUp() {
-        this.decks = new Decks(this.deckRepository);
+        this.deckService = new DeckService(this.deckRepository);
     }
 
     @Test
-    public void should_create_an_empty_deck() {
-        final String id = this.decks.create("My first deck");
+    public void createAnEmptyDeck() {
+        final String id = this.deckService.create("My first deck");
         assertNotNull(id);
         verify(this.deckRepository, times(1))
                 .save(any());
     }
 
     @Test
-    public void should_create_decks_with_different_id() {
-        final String firstDeckId = this.decks.create("My first deck");
-        final String secondDeckId = this.decks.create("My first deck");
+    public void createDecksWithDifferentId() {
+        final String firstDeckId = this.deckService.create("My first deck");
+        final String secondDeckId = this.deckService.create("My first deck");
         assertNotEquals(firstDeckId, secondDeckId);
     }
 
     @Test
-    public void should_remove_a_deck_when_it_exists() {
-        final String deckId = this.decks.create("My first deck");
-        this.decks.remove(deckId);
+    public void removeADeck_when_itExists() {
+        final String deckId = this.deckService.create("My first deck");
+        this.deckService.remove(deckId);
         verify(this.deckRepository, times(1))
                 .delete(any());
     }
