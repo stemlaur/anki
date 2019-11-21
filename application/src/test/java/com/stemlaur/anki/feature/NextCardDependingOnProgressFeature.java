53a71159bcb7beba4824fe519a11a4645caafc75
laurent stemmer <laurent.stemmer@weekendesk.fr>
2019-11-21 13:21:00 +0100

    Write a feature NextCardDependingOnProgressFeature

---
+++ application/src/test/java/com/stemlaur/anki/feature/NextCardDependingOnProgressFeature.java
@@ -0,0 +1,55 @@
+package com.stemlaur.anki.feature;
+
+import com.stemlaur.anki.application.infrastructure.InMemoryCardProgressRepository;
+import com.stemlaur.anki.application.infrastructure.InMemoryDeckRepository;
+import com.stemlaur.anki.application.infrastructure.InMemorySessionRepository;
+import com.stemlaur.anki.domain.catalog.CardDetail;
+import com.stemlaur.anki.domain.catalog.DeckService;
+import com.stemlaur.anki.domain.common.Clock;
+import com.stemlaur.anki.domain.study.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.time.Duration;
+import java.util.Optional;
+
+import static java.time.temporal.ChronoUnit.SECONDS;
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class NextCardDependingOnProgressFeature {
+    private static final String DECK_TITLE = "Brain and its mysteries";
+    private static final String A_QUESTION = "What part of the brain is primarily involved in visual perception ?";
+    private static final String AN_ANSWER = "Occipital lobe";
+    private static final String ANOTHER_QUESTION = "What is the lunate sulcus ?";
+    private static final String ANOTHER_ANSWER = "A fissure in the occipital lobe.";
+
+    private DeckStudyService deckStudyService;
+    private CardProgressService cardProgressService;
+    private String sessionId;
+
+    @Before
+    public void setUp() {
+        final DeckService deckService = new DeckService(new InMemoryDeckRepository());
+        this.cardProgressService = new CardProgressService(new InMemoryCardProgressRepository());
+        this.deckStudyService = new DeckStudyService(deckService, this.cardProgressService, new SessionIdFactory(), new InMemorySessionRepository(), new Clock());
+
+        final String deckId = deckService.create(DECK_TITLE);
+        deckService.addCard(deckId, new CardDetail(A_QUESTION, AN_ANSWER));
+        deckService.addCard(deckId, new CardDetail(ANOTHER_QUESTION, ANOTHER_ANSWER));
+        this.sessionId = this.deckStudyService.startStudySession(deckId);
+    }
+
+    @Test
+    public void nextCardShouldDependOnTheProgress() {
+        final CardToStudy firstCard = this.deckStudyService.nextCardToStudy(sessionId).orElseThrow();
+        this.deckStudyService.study(sessionId, firstCard.id(), Opinion.GREEN);
+        final CardToStudy secondCard = this.deckStudyService.nextCardToStudy(sessionId).orElseThrow();
+
+        assertNotEquals(firstCard.id(), secondCard.id());
+        assertNotEquals(firstCard.question(), secondCard.question());
+    }
+}
