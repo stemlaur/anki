cfe4e2dca01f2d2cced527c21f7fc605f0b3c57d
laurent stemmer <laurent.stemmer@weekendesk.fr>
2019-12-02 15:11:33 +0100

    Improve catalog API living documentation

--- rest/README.md
+++ rest/README.md
@@ -8,20 +8,20 @@ http://localhost:8080/swagger-ui.html#/
 
 ### Be able to find all decks
 ```bash
-> curl -X GET 'http://localhost:8080/decks/'
+curl -X GET 'http://localhost:8080/api/decks' -vvv
 ```
 
 ### Be able to create a deck
 ```bash
-curl -X POST --header "Content-Type: application/json" 'http://localhost:8080/decks/' -d '{ "title": "a deck" }'
+curl -X POST --header "Content-Type: application/json" 'http://localhost:8080/api/decks' -d '{ "title": "a deck" }' -vvv
 ```
 
 ### Be able to add a card to a deck
 ```bash
-curl -X POST --header "Content-Type: application/json" 'http://localhost:8080/decks/876afea3-e297-4366-a91f-01355267171d/card' -d '{ "question": "first question ?", "answer": "first answer" }'
+curl -X POST --header "Content-Type: application/json" 'http://localhost:8080/api/decks/876afea3-e297-4366-a91f-01355267171d/card' -d '{ "question": "first question ?", "answer": "first answer" }' -vvv
 ```
 
 ### Be able to find a deck by id
 ```bash
-curl -X GET --header "Content-Type: application/json" 'http://localhost:8080/decks/876afea3-e297-4366-a91f-01355267171d'
+curl -X GET --header "Content-Type: application/json" 'http://localhost:8080/api/decks/876afea3-e297-4366-a91f-01355267171d' -vvv
 ```
--- rest/src/main/java/com/stemlaur/anki/rest/controllers/catalog/DeckController.java
+++ rest/src/main/java/com/stemlaur/anki/rest/controllers/catalog/DeckController.java
@@ -3,6 +3,10 @@ package com.stemlaur.anki.rest.controllers.catalog;
 import com.stemlaur.anki.domain.catalog.CardDetail;
 import com.stemlaur.anki.domain.catalog.Deck;
 import com.stemlaur.anki.domain.catalog.DeckService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +18,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping(path = "/decks")
+@RequestMapping(path = "/api")
 @Slf4j
 class DeckController {
     private final DeckService deckService;
@@ -23,7 +27,12 @@ class DeckController {
         this.deckService = deckService;
     }
 
-    @GetMapping(path = "/", produces = "application/json")
+    @ApiOperation(value = "Webservice to find all existing decks", response = List.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 500, message = "An internal server error occured")
+    })
+    @GetMapping(path = "/decks", produces = "application/json")
     ResponseEntity<List<DeckDTO>> findAll() {
         try {
             return ResponseEntity.ok(
@@ -36,8 +45,15 @@ class DeckController {
         }
     }
 
-    @PostMapping(path = "/", consumes = "application/json", produces = "application/json")
-    ResponseEntity<String> createDeck(@RequestBody final CreateDeckRequest request) {
+    @ApiOperation(value = "Webservice to create a deck", response = String.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Successfully created and id returned"),
+            @ApiResponse(code = 500, message = "An internal server error occured")
+    })
+    @PostMapping(path = "/decks", consumes = "application/json", produces = "application/json")
+    ResponseEntity<String> createDeck(
+            @ApiParam(value = "Create deck request object", required = true)
+            @RequestBody final CreateDeckRequest request) {
         try {
             final String id = this.deckService.create(request.getTitle());
             URI location = ServletUriComponentsBuilder.fromCurrentRequest()
@@ -51,8 +67,16 @@ class DeckController {
         }
     }
 
-    @PostMapping(path = "/{id}/card", consumes = "application/json", produces = "application/json")
-    ResponseEntity<?> addCard(@PathVariable("id") final String deckId, @RequestBody final AddCardRequest addCardRequest) {
+    @ApiOperation(value = "Webservice to add a card to an existing deck", response = String.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully added the card"),
+            @ApiResponse(code = 404, message = "Deck not found"),
+            @ApiResponse(code = 500, message = "An internal server error occured")
+    })
+    @PostMapping(path = "/decks/{id}/card", consumes = "application/json", produces = "application/json")
+    ResponseEntity<?> addCard(
+            @ApiParam(value = "The id of the deck", required = true) @PathVariable("id") final String deckId,
+            @ApiParam(value = "Add card request object", required = true) @RequestBody final AddCardRequest addCardRequest) {
         try {
             this.deckService.addCard(deckId, new CardDetail(addCardRequest.getQuestion(), addCardRequest.getAnswer()));
             return ResponseEntity.ok().build();
@@ -63,14 +87,25 @@ class DeckController {
         }
     }
 
-    @GetMapping(path = "/{id}", produces = "application/json")
-    ResponseEntity<?> findDeckById(@PathVariable("id") final String deckId) {
-        final Optional<Deck> optionalDeckById = this.deckService.findDeckById(deckId);
-        if (optionalDeckById.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        } else {
-            final Deck deck = optionalDeckById.orElseThrow();
-            return ResponseEntity.ok().body(new DeckDTO(deck.id(), deck.title()));
+    @ApiOperation(value = "Webservice to find an existing deck by its id", response = Deck.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully found"),
+            @ApiResponse(code = 404, message = "Deck not found"),
+            @ApiResponse(code = 500, message = "An internal server error occured")
+    })
+    @GetMapping(path = "/decks/{id}", produces = "application/json")
+    ResponseEntity<?> findDeckById(
+            @ApiParam(value = "The id of the deck", required = true) @PathVariable("id") final String deckId) {
+        try {
+            final Optional<Deck> optionalDeckById = this.deckService.findDeckById(deckId);
+            if (optionalDeckById.isEmpty()) {
+                return ResponseEntity.notFound().build();
+            } else {
+                final Deck deck = optionalDeckById.orElseThrow();
+                return ResponseEntity.ok().body(new DeckDTO(deck.id(), deck.title()));
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(500).build();
         }
     }
 }
