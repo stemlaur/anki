df896acea6722ec83115a0876cc2cb3349cff4f9
Laurent Stemmer <stemlaur@users.noreply.github.com>
2022-01-13 18:05:19 +0100

    Use jooq (#163)
    * Déplace SQLDecks dans son propre package catalog
    
    * Remove unecessary @ComponentScan
    
    * Configure all provider beans
    
    * Make swagger work again !
    
    * Adapte les features pour utiliser les providers en mémoire
    
    Co-authored-by: Laurent Stemmer <laurent-externe.stemmer@edf.fr>

--- application/src/test/java/com/stemlaur/anki/feature/common/TestConfiguration.java
+++ application/src/test/java/com/stemlaur/anki/feature/common/TestConfiguration.java
@@ -1,6 +1,13 @@
 package com.stemlaur.anki.feature.common;
 
+import com.stemlaur.anki.domain.catalog.Decks;
+import com.stemlaur.anki.domain.catalog.fake.InMemoryDecks;
+import com.stemlaur.anki.domain.study.CardProgresses;
+import com.stemlaur.anki.domain.study.Sessions;
+import com.stemlaur.anki.domain.study.fake.InMemoryCardProgresses;
+import com.stemlaur.anki.domain.study.fake.InMemorySessions;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.test.web.servlet.MockMvc;
 
 public class TestConfiguration {
@@ -18,5 +25,23 @@ public class TestConfiguration {
     @Bean
     StudyHttpClient studyHttpClient(final MockMvc mockMvc) {
         return new StudyHttpClient(mockMvc);
+    }
+
+    @Primary
+    @Bean
+    Decks inMemorySqlDecks() {
+        return new InMemoryDecks();
+    }
+
+    @Primary
+    @Bean
+    CardProgresses inMemoryCardProgresses() {
+        return new InMemoryCardProgresses();
+    }
+
+    @Primary
+    @Bean
+    Sessions inMemorySessions() {
+        return new InMemorySessions();
     }
 }
