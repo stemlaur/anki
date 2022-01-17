ffb514596dc9fb40d9c0acfeb0e71ca683035705
Laurent Stemmer <laurent-externe.stemmer@edf.fr>
2022-01-17 13:21:53 +0100

    Remove unused import

--- application/src/main/java/com/stemlaur/anki/application/controllers/LivingDocumentationConfiguration.java
+++ application/src/main/java/com/stemlaur/anki/application/controllers/LivingDocumentationConfiguration.java
@@ -9,8 +9,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.function.Predicate;
-
 @EnableSwagger2
 @Configuration
 class LivingDocumentationConfiguration {
